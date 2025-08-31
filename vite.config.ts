5913522c3a25874f7137aca5f8c7cee68938faa3
wesleyap2014-jpg <wesleyap2014@gmail.com>
2025-08-30 23:45:46 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
+
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: 'dist' }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // agora o Vite entende "@/..."
+    },
+  },
+  build: { outDir: 'dist' },
 })

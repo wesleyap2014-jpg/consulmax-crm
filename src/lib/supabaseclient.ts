5f5cca1e1269d72fd794eed21d71a0993d42f308
wesleyap2014-jpg <wesleyap2014@gmail.com>
2025-08-31 19:25:17 -0400

    Delete src/lib/supabaseclient.ts

--- src/lib/supabaseclient.ts
+++
@@ -1,8 +0,0 @@
-// src/lib/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js';
-
-const url = import.meta.env.VITE_SUPABASE_URL!;
-const anon = import.meta.env.VITE_SUPABASE_ANON_KEY!;
-
-const supabase = createClient(url, anon);
-export default supabase;

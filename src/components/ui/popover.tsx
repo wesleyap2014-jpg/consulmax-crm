665d419e49455847e118fa4c82f8ead07dca26cf
wesleyap2014-jpg <wesleyap2014@gmail.com>
2025-10-04 23:56:29 -0400

    Create popover.tsx

---
+++ src/components/ui/popover.tsx
@@ -0,0 +1,41 @@
+// src/components/ui/popover.tsx
+import * as React from "react";
+import * as PopoverPrimitive from "@radix-ui/react-popover";
+
+// utilzinha básica pra juntar classes (evita depender de "@/lib/utils")
+function cn(...inputs: Array<string | false | null | undefined>) {
+  return inputs.filter(Boolean).join(" ");
+}
+
+const Popover = PopoverPrimitive.Root;
+const PopoverTrigger = PopoverPrimitive.Trigger;
+
+const PopoverContent = React.forwardRef<
+  React.ElementRef<typeof PopoverPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content> & { align?: "start" | "center" | "end" }
+>(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
+  <PopoverPrimitive.Portal>
+    <PopoverPrimitive.Content
+      ref={ref}
+      align={align}
+      sideOffset={sideOffset}
+      className={cn(
+        // base (similar ao shadcn)
+        "z-50 rounded-md border bg-popover p-2 text-popover-foreground shadow-md outline-none",
+        // animações (opcional)
+        "data-[state=open]:animate-in data-[state=closed]:animate-out",
+        "data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+        "data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
+        "data-[side=bottom]:slide-in-from-top-2",
+        "data-[side=left]:slide-in-from-right-2",
+        "data-[side=right]:slide-in-from-left-2",
+        "data-[side=top]:slide-in-from-bottom-2",
+        className
+      )}
+      {...props}
+    />
+  </PopoverPrimitive.Portal>
+));
+PopoverContent.displayName = "PopoverContent";
+
+export { Popover, PopoverTrigger, PopoverContent };

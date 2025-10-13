e810971e43e48cc812d8e30c3651c1a8f7e61355
wesleyap2014-jpg <wesleyap2014@gmail.com>
2025-10-12 23:50:30 -0400

    Update Simuladores.tsx

--- src/pages/Simuladores.tsx
+++ src/pages/Simuladores.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Loader2, Plus, Pencil, Trash2, X } from "lucide-react";
+import { Loader2, Plus, Pencil, Trash, X, Settings } from "lucide-react";
 import { useParams, useLocation, useSearchParams } from "react-router-dom";
 import { ChevronsUpDown, Search } from "lucide-react";
 import { Popover, PopoverButton, PopoverContent, PopoverClose } from "@/components/ui/popover";

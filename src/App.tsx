f310f8ab1450cc8d71115ea995350df59ca5c6dd
wesleyap2014-jpg <wesleyap2014@gmail.com>
2025-10-25 01:39:19 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,23 @@ export default function App() {
   return (
     <div className="min-h-screen">
       <Header />
+
       <div className="flex">
         <Sidebar />
-        <main className="flex-1 p-4">
-          <Outlet />
+
+        {/* área central com o liquid glass de fundo */}
+        <main className="relative flex-1 p-4 isolate">
+          {/* fundo líquido: primeiro filho do container relative */}
+          <div className="liquid-bg">
+            <span className="blob b1" />
+            <span className="blob b2" />
+            <span className="gold" />
+          </div>
+
+          {/* conteúdo acima do fundo */}
+          <div className="relative z-[1]">
+            <Outlet />
+          </div>
         </main>
       </div>
     </div>

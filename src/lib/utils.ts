874d34ef8fe7a3f83cc2adb5948c53f77ebf42a1
wesleyap2014-jpg <wesleyap2014@gmail.com>
2025-11-11 01:03:58 -0400

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,11 @@
 // src/lib/utils.ts
-import { type ClassValue, clsx } from "clsx";
+import clsx, { type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 /** Merge de classes Tailwind com segurança (padrão shadcn/ui) */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+/** Alias útil quando quiser apenas concatenar sem merge do Tailwind */
+export const cx = (...inputs: ClassValue[]) => clsx(inputs);

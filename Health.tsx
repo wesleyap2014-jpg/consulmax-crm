497fb3c714ce3b68501a61ef244f5f279966ef8f
wesleyap2014-jpg <wesleyap2014@gmail.com>
2025-11-11 21:31:00 -0400

    Delete Health.tsx

--- Health.tsx
+++
@@ -1,9 +0,0 @@
-// src/pages/Health.tsx
-export default function Health() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1 className="text-xl font-bold">✅ Health OK</h1>
-      <p>Router, React e build estão funcionando.</p>
-    </div>
-  );
-}

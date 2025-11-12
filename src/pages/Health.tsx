bb8537b36ec8bf6fcffcb2823153bf4e1a2d700d
wesleyap2014-jpg <wesleyap2014@gmail.com>
2025-11-11 21:43:08 -0400

    Create Health.tsx

---
+++ src/pages/Health.tsx
@@ -0,0 +1,8 @@
+// src/pages/Health.tsx
+export default function Health() {
+  return (
+    <div style={{ padding: 16, fontFamily: "Inter, system-ui" }}>
+      <strong>OK</strong> — Health page carregada.
+    </div>
+  );
+}
